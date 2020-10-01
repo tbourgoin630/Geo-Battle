f3914af026757d3f7f47c986fbce4c9efb41731c
Jesse Farmer <drjfarmer@gmail.com>
2020-09-30 21:35:13 -0400

    battle scene

--- app/public/js/battle.js
+++ app/public/js/battle.js
@@ -4,6 +4,30 @@
 
 
 let health = document.getElementById("healthplayer")
-health.value -= 10;
-console.log(health.value)
+health.value -= Math.random();
+console.log(health)
 //Or whatever you want to do with it.
+
+document.getElementById("heroCurrentHp").innerHTML = "Current HP: " + hero.stats.currentHp;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//when the health hits zero this will go into effect. 
+document.getElementById("characterCreation").style.display = "none";
