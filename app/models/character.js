8459d34de234f962d06c0f05ec5c2e94eef29f6c
Matt Hunt <wmatthunt@gmail.com>
2020-10-01 20:43:18 -0400

    href not working

--- app/models/character.js
+++ app/models/character.js
@@ -26,53 +26,7 @@ var Battler = sequelize.define("battler_db", {
             speed: Sequelize.INTEGER,
             damage: Sequelize.INTEGER,
             player: Sequelize.BOOLEAN,
-        // checkIfHit(target) {
-        //     let hitChance = .5 - .1 * (target.defense);
-        //     console.info(hitChance + " hit chance")
-        //     let hitRoll = Math.random() + .1 * (this.attack);
-        //     console.info(hitRoll + " hit roll")
-        //     if (hitRoll >= hitChance) {
-        //         hit = true;
-        //     }
-        //     else {
-        //         hit = false;
-    
-        //     }
-        // },
-        // attackTarget(target) {
-        //     console.info(this.name + " attacks " + target.name)
-        //     this.checkIfHit(target)
-        //     if (hit === true) {
-        //         let dmg = this.damage
-        //         console.info(dmg)
-        //         target.hitPoints -= dmg;
-        //     }
-        //     else {
-        //         console.info(this.name + " misses " + target.name)
-        //     }
-        //     target.isAlive();
-        // },
-        // isAlive() {
-        //     if (this.hitPoints > 0) {
-        //         console.info(this.name + " is alive.")
-        //     }
-        //     else {
-        //         console.info(this.name + " has died!");
-        //     }
-        // },
-        // turn() {
-        //     if (this.hitPoints > 0) {
-        //         if (this.player === false) {
-        //             this.attackTarget(hero)
-        //         }
-        //         else {
-        //             this.attackTarget(enemy)
-        //         }
-        //     }
-        //     else {
-        //         console.info("no turn for dead person")
-        //     }
-        // }
+        
 });
 
 // Syncs with DB
