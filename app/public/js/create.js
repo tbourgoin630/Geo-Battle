be3093435ee5c4df83dc48cecb8b2272629d4cde
jackie <mvpbremah@yahoo.com>
2020-10-01 20:47:38 -0400

    commit

--- app/public/js/create.js
+++ app/public/js/create.js
@@ -1,5 +1,5 @@
-function createBattler()
-{
+function createBattler() {
+   
     let name = document.getElementById("warrior").value;
     //let hitPoints = document.getElementById("hp").value;
     let attack = document.getElementById("attack").value;
@@ -8,6 +8,20 @@ function createBattler()
     let speed = document.getElementById("speed").value;
 
     //hero creation
-    hero = new Character (name, 10, attack, defense, damage, speed, true)
+    hero = new Character(name, 10, attack, defense, damage, speed, true)
     console.log(hero)
+    
+   var myhero = {name, attack, defense, damage, speed }
+    var myJSON = JSON.stringify(hero);
+
+    var query = connection.query('INSERT INTO table SET column=?', responseJson, 
+     function(err, result) {
+
+        if(err) throw err;
+        
+       console.log('data inserted');
+ });
+
+
 }
+
