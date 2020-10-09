2f815c39bf4c654bbdefa26f027bacc8472f9a43
jackie <mvpbremah@yahoo.com>
2020-10-08 23:36:09 -0400

    commit

--- app/configs/connection.js
+++ app/configs/connection.js
@@ -1,3 +1,4 @@
+const Connection = require("mysql2/typings/mysql/lib/Connection");
 // *********************************************************************************
 // CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
 // *********************************************************************************
@@ -5,6 +6,17 @@
 // Dependencies
 var Sequelize = require("sequelize");
 
+if (process.env.JAWSDB_URL) {
+  Connection = mysql.createConnection(process.env.JAWSDB_URL);
+  
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "hacktheplanet",
+    database: "todoagain_db"
+  }) 
+}
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
 var sequelize = new Sequelize("battler_db", "root", "", {
   host: "localhost",
